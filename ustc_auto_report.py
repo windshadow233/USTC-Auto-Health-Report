e222448ca5a6450d9a7473fdc1fcc30616e30d32
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2021-09-15 17:37:19 +0800

    Update ustc_auto_report.py

--- ustc_auto_report.py
+++ ustc_auto_report.py
@@ -1,18 +1,15 @@
-import requests
 import json
 from bs4 import BeautifulSoup
-import pytesseract
-from PIL import Image
-import numpy as np
-import cv2
-import time
 import datetime
-import os
+
+from ustc_passport_login import USTCPassportLogin
 
 
 class USTCAutoHealthReport(object):
     def __init__(self):
-        self.sess = requests.session()
+        # 用于登录
+        self.login_bot = USTCPassportLogin()
+        self.sess = self.login_bot.sess
         self.url = 'https://passport.ustc.edu.cn/login?service=https%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin'
         # 登录url
         self.login_url = 'https://passport.ustc.edu.cn/login'
@@ -22,51 +19,17 @@ class USTCAutoHealthReport(object):
         self.clock_in_url = 'https://weixine.ustc.edu.cn/2020/daliy_report'
         # 报备url
         self.report_url = 'https://weixine.ustc.edu.cn/2020/apply/daliy/post'
-        # 验证码图片存放路径
-        self.LT_save_path = '/tmp'
-        # 验证码图片文件名
-        self.LT_file = ''
-        # 验证码识别结果
-        self.LT = ''
         # 身份认证token
         self.token = ''
 
-    def _get_CAS_LT(self):
-        """
-        获取登录时需要提供的验证字段
-        """
-        response = self.sess.get(self.url)
-        CAS_LT = BeautifulSoup(response.text, 'lxml').find(attrs={'id': 'CAS_LT'}).get('value')
-        return CAS_LT
-
-    def _get_token(self, response):
+    def _get_token(self):
         """
         获取打卡时需要提供的验证字段
         """
+        response = self.sess.get(self.url)
         s = BeautifulSoup(response.text, 'lxml')
         token = s.find(attrs={'name': '_token'}).get('value')
         return token
-
-    def _save_validate_number(self):
-        """
-        将验证码图片保存到一个文件
-        """
-        validate_number = self.sess.get(self.validate_url)
-        self.LT_file = str(time.time()) + '.jpg'
-        with open(os.path.join(self.LT_save_path, self.LT_file), 'wb') as f:
-            f.write(validate_number.content)
-
-    def _recognize_validate_number(self):
-        """
-        识别验证码
-        """
-        image = cv2.imread(os.path.join(self.LT_save_path, self.LT_file))
-        kernel = np.ones((2, 2), np.uint8)
-        image = cv2.dilate(image, kernel, iterations=1)
-        image = Image.fromarray(image).convert('L')
-        config = '--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789'
-        self.LT = pytesseract.image_to_string(image, config=config).strip()
-        return self.LT
 
     def _check_success(self, response):
         """
@@ -76,33 +39,12 @@ class USTCAutoHealthReport(object):
 
     def login(self, username, password):
         """
-        登录,需要提供用户名、密码，记录下token
+        登录,需要提供用户名、密码
         """
-        self.sess.cookies.clear()
-        self.LT_file = ''
-        self.LT = ''
         self.token = ''
-        try:
-            CAS_LT = self._get_CAS_LT()
-            self._save_validate_number()
-            LT = self._recognize_validate_number()
-            login_data = {
-                'username': username,
-                'password': password,
-                'warn': '',
-                'CAS_LT': CAS_LT,
-                'showCode': '1',
-                'button': '',
-                'model': 'uplogin.jsp',
-                'service': 'https://weixine.ustc.edu.cn/2020/caslogin',
-                'LT': LT
-            }
-            response = self.sess.post(self.login_url, login_data)
-            token = self._get_token(response)
-            self.token = token
-            return True
-        except:
-            return False
+        is_success = self.login_bot.login(username, password)
+        self.token = self._get_token()
+        return is_success
 
     def daily_clock_in(self, post_data_file):
         """
