c39348073d5374c860e03e12a32458a0da0501a1
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2022-06-06 14:40:26 +0800

    增加行程码自动生成、上传功能

--- ustc_auto_report.py
+++ ustc_auto_report.py
@@ -1,6 +1,12 @@
 import json
 from bs4 import BeautifulSoup
 import datetime
+import time
+from PIL import ImageFont, Image, ImageDraw
+import os
+import random
+import re
+import io
 
 from ustc_passport_login import USTCPassportLogin
 
@@ -16,6 +22,10 @@ class USTCAutoHealthReport(object):
         self.clock_in_url = 'https://weixine.ustc.edu.cn/2020/daliy_report'
         # 每周报备url
         self.report_url = 'https://weixine.ustc.edu.cn/2020/apply/daliy/ipost'
+        # 图片上传get url
+        self.upload_url = 'https://weixine.ustc.edu.cn/2020/upload/xcm'
+        # 图片上传post url
+        self.upload_image_url = 'https://weixine.ustc.edu.cn/2020img/api/upload_for_student'
         # 每日进出校申请url
         self.stayinout_apply_url = 'https://weixine.ustc.edu.cn/2020/apply/daliy/ipost'
         # 身份认证token
@@ -34,7 +44,41 @@ class USTCAutoHealthReport(object):
         """
         简单check一下有没有成功打卡、报备
         """
-        return '成功' in response.text
+        s = BeautifulSoup(response.text, 'lxml')
+        msg = s.select('.alert')[0].text
+        return '成功' in msg
+
+    def _generate_xing_cheng_ma(self, phone_number):
+        dir_path = os.path.dirname(os.path.abspath(__file__))
+        img_pil = Image.open(os.path.join(dir_path, "xcm/blank_xcm.jpg")).convert('RGBA')
+        time_font = ImageFont.truetype(os.path.join(dir_path, "xcm/fonts/arial.ttf"), 33)
+        mobile_number_font = ImageFont.truetype(os.path.join(dir_path, "xcm/fonts/arialbd.ttf"), 27)
+        draw = ImageDraw.Draw(img_pil)
+        draw.text((242, 342), time.strftime("%Y.%m.%d %H:%M:%S", time.localtime()), (0x94, 0x94, 0x9e), time_font)
+        draw.text((178, 283), f'{phone_number[:3]}****{phone_number[-4:]}', (0x46, 0x46, 0x4c), mobile_number_font)
+
+        arrow = Image.open(os.path.join(dir_path, "xcm/gif_green", random.choice(os.listdir(os.path.join(dir_path, "xcm/gif_green")))))
+        r, g, b, a = arrow.split()
+        img_pil.paste(arrow, (180, 400), mask=a)
+        return img_pil
+
+    def _get_gid_sign(self):
+        r = self.sess.get(self.upload_url)
+        gid = re.search("'gid': '(.*)'", r.text).groups()[0]
+        sign = re.search("'sign': '(.*)'", r.text).groups()[0]
+        return gid, sign
+
+    def _upload_xing_cheng_ma(self, image):
+        gid, sign = self._get_gid_sign()
+        output = io.BytesIO()
+        image.save(output, format='PNG')
+        image = output.getvalue()
+        data = {"_token": "", "gid": gid, "sign": sign, "t": 1, "id": "WU_FILE_0",
+                "name": "Screenshot_Wechat.png", "type": "image/png",
+                "file": "", 'size': len(image)}
+        files = {'file': ("Screenshot_Wechat.png", image, "image/png", {})}
+        r = self.sess.post(self.upload_image_url, data=data, files=files)
+        return r.json()['status']
 
     def login(self, username, password):
         """
@@ -84,13 +128,19 @@ class USTCAutoHealthReport(object):
             print(e)
             return 0
 
-    def stayinout_apply(self, apply_data_file):
+    def stayinout_apply(self, apply_data_file, phone_number=None):
         """
         2022年3月18日起每日进出校申请
         申请成功返回True,申请失败返回False
         :param apply_data_file表单数据文件
+        :param phone_number手机号(用以生成行程码,若不提供则不自动上传行程码)
         """
         try:
+            if phone_number is not None:
+                xcm = self._generate_xing_cheng_ma(phone_number)
+                status = self._upload_xing_cheng_ma(xcm)
+                if not status:
+                    return False
             with open(apply_data_file, 'r') as f:
                 post_data = json.loads(f.read())
             now = datetime.datetime.now()
